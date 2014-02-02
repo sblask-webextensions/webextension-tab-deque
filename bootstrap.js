a29fdb8a0cb7ddadeb9142af7b5124492904a0dd
Sebastian Blask <blask@gmx.de>
2014-02-02 11:05:37 +0100

    Add bootstrap.js

---
+++ bootstrap.js
@@ -0,0 +1,60 @@
+Components.utils.import("resource://gre/modules/Services.jsm");
+Components.utils.import("resource://gre/modules/devtools/Console.jsm");
+
+var initFunction = function(domWindow) {
+    var extensionResource = "chrome://tabdeque/content/tabdeque.jsm";
+    Components.utils.import(extensionResource, domWindow);
+    domWindow.gTabDeque.initialize(domWindow);
+}
+
+var destroyFunction = function(domWindow) {
+    domWindow.gTabDeque.destroy();
+    domWindow.gTabDeque = undefined;
+}
+
+function simpleToDomWindow(aWindow) {
+    return aWindow.QueryInterface(Components.interfaces.nsIDOMWindow);
+}
+
+function toDomWindow(aWindow) {
+    return aWindow
+              .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+              .getInterface(Components.interfaces.nsIDOMWindowInternal ||
+                  Components.interfaces.nsIDOMWindow);
+}
+
+var windowListener = {
+    onOpenWindow: function (aWindow) {
+        var domWindow = toDomWindow(aWindow);
+        var onLoadFunction = function() {
+            domWindow.removeEventListener("load", arguments.callee, false);
+            initFunction(domWindow);
+        }
+        domWindow.addEventListener("load", onLoadFunction, false);
+    },
+    onCloseWindow: function (aWindow) {},
+    onWindowTitleChange: function (aWindow, aTitle) {}
+}
+
+var windowMediator =
+    Components.classes["@mozilla.org/appshell/window-mediator;1"]
+        .getService(Components.interfaces.nsIWindowMediator);
+
+function callOnOpenWindows(someFunction) {
+    var openWindows = windowMediator.getEnumerator("navigator:browser");
+    while (openWindows.hasMoreElements()) {
+        someFunction(simpleToDomWindow(openWindows.getNext()));
+    }
+}
+
+function install() {}
+function uninstall() {}
+function startup(data, reason) {
+    callOnOpenWindows(initFunction);
+    windowMediator.addListener(windowListener);
+}
+function shutdown(data, reason) {
+    windowMediator.removeListener(windowListener);
+    callOnOpenWindows(destroyFunction);
+}
+
